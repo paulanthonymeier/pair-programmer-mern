16dad7137f7e6fe27605aa304877b4f032de88de
maaronkel <mdevk@posteo.net>
2021-01-13 09:53:24 +1100

    install passport and bcrypt

--- src/server/index.js
+++ src/server/index.js
@@ -28,7 +28,7 @@ const CONNECTION_URL = 'mongodb+srv://mattk:2049goose@cluster0.yd6ia.mongodb.net
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .then(() => app.listen(PORT, () => console.log(`Connected to MongoDB and Server running on port ${PORT}`)))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
